4d69245da1b979d7da7f2303d1cf941bc4f4317f
Dara Kosova <darakosova.gb@gmail.com>
2022-06-30 23:15:33 +0400

    Выполнили 5-ю задачу

---
+++ task_2_ArrayLength/Program.cs
@@ -0,0 +1,55 @@
+﻿//массив заполняемый в случайном порядке числами от 1-го до 10-ти
+//            создай новый массив, в кот-м будет 10 эл-в
+
+void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        //обратиться к аргументу коллекции на позицию индекс и положить туда новое целое число из диап-на 1-10
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //-1, чтобы недопустимый элемент не совпадал с индексом позиции нахождения числа в массиве
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array); //заполнил массив
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
